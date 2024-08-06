717a38b7931858a89735f3819152df74a461b961
morceli <109186798+joaotds@users.noreply.github.com>
2024-08-06 11:30:15 -0300

    Create soma.php

---
+++ soma.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "dados recebidos por GET". "<br>";
+
+$numero1 = $_GET["numero1"];
+echo "Numero 1: " . $numero1 . "<br>";
+
+$numero2 = $_GET["numero2"];
+echo "Numero 2: " . $numero2 . "<br>";
+
+echo "Soma: " . $numero1 + $numero2 . "<br>";
+
+
