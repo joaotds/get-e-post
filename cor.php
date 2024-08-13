2828fb33ded942cb70c11dd9732075f070b70ecf
morceli <109186798+joaotds@users.noreply.github.com>
2024-08-13 09:54:54 -0300

    Create cor.php

---
+++ cor.php
@@ -0,0 +1,13 @@
+<?php
+
+$cor = $_GET["cor"] ?? "white";
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Cor de Fundo</title>
+</head>
+<body style="background-color: <?php echo $cor; ?>;">
+</body>
+</html>
